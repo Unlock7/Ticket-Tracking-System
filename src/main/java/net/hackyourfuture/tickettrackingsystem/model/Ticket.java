539461463ca7e583067988eae855a414a28a331b
Dagim02 <dagimvanhaileselassie@gmail.com>
2026-06-16 12:30:57 +0200

    feat: implement tickets

--- src/main/java/net/hackyourfuture/tickettrackingsystem/model/Ticket.java
+++ src/main/java/net/hackyourfuture/tickettrackingsystem/model/Ticket.java
@@ -1,4 +1,19 @@
 package net.hackyourfuture.tickettrackingsystem.model;
 
+import lombok.*;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Ticket {
+    private Long id;
+    private String title;
+    private String description;
+    private String status;     // open, in progress, closed
+    private Long projectId;
+    private Instant createdAt;
 }
+
